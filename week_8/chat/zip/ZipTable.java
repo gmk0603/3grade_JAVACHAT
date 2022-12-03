128da8ff49cd9a4b0efc78390081bb25d674d635
gmk0603 <57435225+gmk0603@users.noreply.github.com>
2022-12-04 02:37:10 +0900

    Add files via upload

---
+++ week_8/chat/zip/ZipTable.java
@@ -0,0 +1,38 @@
+package chat.zip;
+
+import javax.swing.table.AbstractTableModel;
+
+public class ZipTable extends AbstractTableModel {
+	String[] columNames =
+		{"일련번호","우편번호","시.도","구.군","동","리","빌딩","번지"};
+	//데이터
+	Object[][] data = {{" ", " "," "," "," "," "," "," "}}; 
+    public ZipTable(){
+          
+    }
+ 
+    public ZipTable(Object[][] data) {
+           this.data = data;
+    }
+ 
+    @Override
+    public int getColumnCount() {
+    	return columNames.length;
+    }
+ 
+    @Override
+    public int getRowCount() {
+    	return data.length;           //2차 배열의 길이
+    }
+ 
+    @Override
+    public Object getValueAt(int arg0, int arg1) {
+        return data[arg0][arg1];
+    }
+ 
+    @Override
+    public String getColumnName(int arg0) {
+    	return columNames[arg0];
+    }
+
+}
